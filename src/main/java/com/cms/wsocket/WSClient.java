37e86bbc0b5090c3bde0f9d06cc289601fce4eba
Chamith Nimmitha <chamith@localhost.localdomain>
2021-10-27 15:13:59 +0530

    create web sockets

---
+++ src/main/java/com/cms/wsocket/WSClient.java
@@ -0,0 +1,30 @@
+package com.cms.wsocket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class WSClient{
+
+    private static Socket clientSocket;
+
+    public static void main(String[] args) throws IOException {
+        try {
+            clientSocket = new Socket("127.0.0.1", 5555);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+
+        InputStream inputStream = clientSocket.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        System.out.println("called");
+        int c;
+        while( (c=bufferedReader.read()) !=-1){
+            System.out.print((char)c);
+        }
+    }
+}
